444913e944d6cbef1ffdf52fe909f6e50044790f
eiwahwin-git <eiwahwin23319@gmail.com>
2021-12-16 13:38:46 -0800

    [main]tutorial 4 finished

--- Tutorials/tutorial_04/login.php
+++ Tutorials/tutorial_04/login.php
@@ -10,7 +10,7 @@ session_start();
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="css/style.css">
-  <title>Tutorial4</title>
+  <title>Tutorial 4</title>
 </head>
 
 <body>
