4e21cb243741493924a893b3fad2fe8f68054fbe
eiwahwin-git <eiwahwin23319@gmail.com>
2021-12-20 12:45:09 -0800

    [main]finished tutorial 6

---
+++ Tutorials/tutorial_06/upload.php
@@ -0,0 +1,25 @@
+<?php
+$foldername = $_POST["foldername"];
+$location = $foldername . "/";
+$fileName = $_FILES["file"]["name"];
+$tmp = $_FILES["file"]["tmp_name"];
+$type = $_FILES["file"]["type"];
+if(isset($fileName))
+{
+  $structure = "./" .$foldername;
+  if (!mkdir($structure)) {
+    echo "Folder Exist";
+  }
+  if(!empty($fileName))
+  {if($type="jpg"||$type="jpeg"||$type="png"){
+    if(move_uploaded_file($tmp, $location.$fileName))
+    {
+      echo "File Uploaded " .$foldername;
+    }
+    else{
+      echo "Not Uploaded  " .$foldername;
+    }
+  }
+}
+}
+?>
