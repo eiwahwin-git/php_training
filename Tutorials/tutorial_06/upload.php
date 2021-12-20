b610c0d77065c38ea401a65cf9362131d9cb6195
eiwahwin-git <eiwahwin23319@gmail.com>
2021-12-20 13:41:54 -0800

    [main] finished tutorial6

--- Tutorials/tutorial_06/upload.php
+++ Tutorials/tutorial_06/upload.php
@@ -4,21 +4,18 @@ $location = $foldername . "/";
 $fileName = $_FILES["file"]["name"];
 $tmp = $_FILES["file"]["tmp_name"];
 $type = $_FILES["file"]["type"];
-if(isset($fileName))
-{
-  $structure = "./" .$foldername;
-  if (!mkdir($structure)) {
-    echo "Folder Already Existed.<br>";
-  }
-  if(!empty($fileName))
-  {if($type="jpg"||$type="jpeg"||$type="png"){
-    if(move_uploaded_file($tmp, $location.$fileName))
-    {
-      echo "File Uploaded to " .$foldername;
+if (isset($fileName)) {
+    $structure = "./" . $foldername;
+    if (!mkdir($structure)) {
+        echo "Folder Already Existed.<br>";
     }
-    else{
-      echo "Not Uploaded  " .$foldername;
+    if (!empty($fileName)) {
+        if ($type = "jpg" || $type = "jpeg" || $type = "png") {
+            if (move_uploaded_file($tmp, $location . $fileName)) {
+                echo "File Uploaded to " . $foldername;
+            } else {
+                echo "Not Uploaded  " . $foldername;
+            }
+        }
     }
-  }
-}
 }
