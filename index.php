168dc22e6b1e868750e1f27fda5195c3f166eabd
eiwahwin-git <eiwahwin23319@gmail.com>
2021-12-22 13:08:26 -0800

    [main]finished tutorial 8

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'phpqrcode/qrlib.php';
-$path="img/";
-$file=$path.uniqid().".png";
-$text="Something";
-QRcode::png($text,$file,'L',10,2);
-//png($text,$file,ECClevel,PiXelsize,Frame_Size);
-echo"<img src='".$file."'>";
-?>
